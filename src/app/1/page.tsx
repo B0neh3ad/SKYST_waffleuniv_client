2dff1564738abebfa1d5949ee517bbd4cffb7326
Junbye <pjb0621@naver.com>
2025-05-10 12:47:16 +0900

    feat: ssr / client 분리

---
+++ src/app/1/page.tsx
@@ -0,0 +1,5 @@
+import TestSection from "@/components/1/TestSection";
+
+export default function Home() {
+  return <TestSection />;
+}
---
+++ src/components/1/TestSection.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+import { useEffect, useState } from "react";
+
+export default function TestSection() {
+  return (
+    <div>
+      <h1>Test Section</h1>
+    </div>
+  );
+}
